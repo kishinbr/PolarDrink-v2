ea81d71ea25fe99632f1237e7e647422249416f1
kishinbr <viniciuskishibr@gmail.com>
2026-09-09 16:16:45 -0300

    feat: adicionando view basica de login e cadastro

---
+++ PolarDrinks/Controllers/Loja/LojaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PolarDrinks.Controllers.Loja
+{
+    public class LojaController : Controller
+    {
+        public IActionResult Catalogo()
+        {
+            return View();
+        }
+
+        public IActionResult Conta()
+        {
+            return View();
+        }
+
+        public IActionResult Carrinho()
+        {
+            return View();
+        }
+    }
+}
